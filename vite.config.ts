b9708f098c7bbc03a1f6d1207a4fdb5c363ed9c6
MohammadHamdanGhanchi <suhnaa87@gmail.com>
2025-09-21 22:29:14 +0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -13,6 +12,11 @@ export default defineConfig(() => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [],
     },
   },
 }));
